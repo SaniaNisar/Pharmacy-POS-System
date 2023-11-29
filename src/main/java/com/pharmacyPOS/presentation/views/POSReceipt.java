3eb59f2168d958988ddc0fec9c5ee6cb8df5797e
Sania Nisar <sania.nisar01@gmail.com>
2023-11-30 01:06:31 +0500

    New Update by Sania

--- src/main/java/com/pharmacyPOS/presentation/views/POSReceipt.java
+++ src/main/java/com/pharmacyPOS/presentation/views/POSReceipt.java
@@ -1,11 +1,17 @@
 package com.pharmacyPOS.presentation.views;
 
+import com.pharmacyPOS.data.dao.InventoryDao;
+import com.pharmacyPOS.data.entities.Inventory;
 import com.pharmacyPOS.data.entities.Order;
 import com.pharmacyPOS.data.entities.OrderDetail;
+import com.pharmacyPOS.data.entities.Product;
 import com.pharmacyPOS.presentation.controllers.CartController;
 
 import javax.swing.*;
 import java.awt.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.List;
@@ -13,12 +19,16 @@ import java.util.List;
 public class POSReceipt extends JFrame {
     private Order order; // Assuming Order is a class that holds order details
     private double paidAmount;
+    private double total;
+
     private String salesAssistantName;
+    private OrderDetail orderDetails;
 
     private CartController cartController;
 
-    public POSReceipt(Order order, double paidAmount, String salesAssistantName) {
+    public POSReceipt(Order order, double total, double paidAmount, String salesAssistantName) {
         this.order = order;
+        this.total = total;
         this.paidAmount = paidAmount;
         this.salesAssistantName = salesAssistantName;
         setupUI();
@@ -51,7 +61,7 @@ public class POSReceipt extends JFrame {
 
 
         int userId = 2;
-        double total = cartController.getCartTotal(userId); // Assuming getTotalAmount() calculates the total
+//        double total = cartController.getCartTotal(userId); // Assuming getTotalAmount() calculates the total
         double change = paidAmount - total;
 
         receiptText.append("\nTotal Amount: ").append(String.format("%.2f", total)).append("\n");
@@ -65,6 +75,46 @@ public class POSReceipt extends JFrame {
 
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         setLocationRelativeTo(null);
+        InvoiceDetails();
         setVisible(true);
     }
+    public void InvoiceDetails() {
+        String fileName = createFileNameWithCurrentDate();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            StringBuilder receiptText = new StringBuilder();
+            receiptText.append("Pharmacy POS Receipt\n");
+            receiptText.append("----------------------------\n");
+            receiptText.append("Order ID: ").append(order.getOrderId()).append("\n");
+            receiptText.append("Date: ").append(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new Date())).append("\n");
+            receiptText.append("Sales Assistant: ").append(salesAssistantName).append("\n\n");
+            receiptText.append("Items:\n");
+
+            // Assuming OrderDetail is a class that holds item details
+            List<OrderDetail> items = List.of(order.getOrderDetails());
+            for (OrderDetail item : items) {
+                receiptText.append(item.getProductId()).append(" x").append(item.getQuantity())
+                        .append(" @ ").append(item.getUnitPrice()).append("\n");
+            }
+
+
+            int userId = 2;
+//            double total = cartController.getCartTotal(userId); // Assuming getTotalAmount() calculates the total
+            double change = paidAmount - total;
+
+            receiptText.append("\nTotal Amount: ").append(String.format("%.2f", total)).append("\n");
+            receiptText.append("Paid Amount: ").append(String.format("%.2f", paidAmount)).append("\n");
+            receiptText.append("Change: ").append(String.format("%.2f", change)).append("\n");
+            writer.write(String.valueOf(receiptText));
+            writer.newLine();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private String createFileNameWithCurrentDate() {
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String currentDate = sdf.format(new Date());
+        return "Invoice Slip - " + currentDate + ".txt";
+    }
+
 }
