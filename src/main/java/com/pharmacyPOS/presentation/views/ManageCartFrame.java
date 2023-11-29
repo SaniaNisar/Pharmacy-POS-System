cc9ca557007c53bc5a296aaae516a05c7c98713b
Sania Nisar <sania.nisar01@gmail.com>
2023-11-29 12:04:47 +0500

    Update by Sania

--- src/main/java/com/pharmacyPOS/presentation/views/ManageCartFrame.java
+++ src/main/java/com/pharmacyPOS/presentation/views/ManageCartFrame.java
@@ -10,9 +10,11 @@ import java.awt.event.ActionListener;
 import java.sql.SQLException;
 
 import com.pharmacyPOS.data.dao.CartDao;
+import com.pharmacyPOS.data.dao.OrderDao;
 import com.pharmacyPOS.data.database.DatabaseConnection;
 import com.pharmacyPOS.data.entities.Cart;
 import com.pharmacyPOS.data.entities.CartItem;
+import com.pharmacyPOS.data.entities.Order;
 import com.pharmacyPOS.presentation.controllers.CartController;
 import com.pharmacyPOS.service.CartService;
 
@@ -22,14 +24,17 @@ public class ManageCartFrame extends JFrame {
     private CartController cartController;
     private JTable cartTable;
     private DefaultTableModel cartTableModel;
-    private JButton checkoutButton;
+    private JButton processOrderButton;
     private int userId;
+    private int currentOrderId; // To keep track of the current order ID
+    private OrderDao orderDao; // Ensure that orderDao is initialized properly
 
     public ManageCartFrame(int userId) {
         this.userId = userId;
         DatabaseConnection c = new DatabaseConnection();
         c.connect();
         this.cartController = new CartController(new CartService(new CartDao(c)));
+        this.orderDao = new OrderDao(c);
 
         setupUI();
         loadCartItems();
@@ -60,9 +65,9 @@ public class ManageCartFrame extends JFrame {
             }
         };
 
-        checkoutButton = new JButton("Checkout");
-        checkoutButton.addActionListener(this::onCheckoutClicked);
-        add(checkoutButton, BorderLayout.SOUTH);
+        processOrderButton = new JButton("Process Order");
+        processOrderButton.addActionListener(this::onProcessOrderClicked);
+        add(processOrderButton, BorderLayout.SOUTH);
 
         add(new JScrollPane(cartTable), BorderLayout.CENTER);
     }
@@ -126,8 +131,7 @@ public class ManageCartFrame extends JFrame {
         cartTableModel.setRowCount(0);
         try {
             List<CartItem> cartItems = cartController.getCartItems(userId);
-            for (CartItem item : cartItems)
-            {
+            for (CartItem item : cartItems) {
                 cartTableModel.addRow(new Object[]{
                         item.getProductId(),
                         item.getProductName(),
@@ -139,16 +143,6 @@ public class ManageCartFrame extends JFrame {
         } catch (SQLException ex) {
             ex.printStackTrace();
             JOptionPane.showMessageDialog(this, "Error loading cart items", "Error", JOptionPane.ERROR_MESSAGE);
-        }
-    }
-
-    private void onCheckoutClicked(ActionEvent e) {
-        try {
-            // Implement checkout logic
-            JOptionPane.showMessageDialog(this, "Checkout successful!", "Success", JOptionPane.INFORMATION_MESSAGE);
-        } catch (Exception ex) {
-            ex.printStackTrace();
-            JOptionPane.showMessageDialog(this, "Checkout failed", "Error", JOptionPane.ERROR_MESSAGE);
         }
     }
 
@@ -174,5 +168,92 @@ public class ManageCartFrame extends JFrame {
         c.connect();
         CartController cartController = new CartController(new CartService(new CartDao(c))); // Initialize with actual cart controller
         new ManageCartFrame(userId);
+    }
+
+    private void onProcessOrderClicked(ActionEvent e) {
+        // Calculate the total amount (replace with actual calculation logic)
+        double totalAmount = calculateTotalAmount();
+
+        // Show the initial processing message
+        JOptionPane.showMessageDialog(this, "Processing your Order!", "Success", JOptionPane.INFORMATION_MESSAGE);
+
+        // Create the payment frame
+        JFrame paymentFrame = new JFrame("Payment");
+        paymentFrame.setSize(300, 200);
+        paymentFrame.setLayout(new FlowLayout());
+        paymentFrame.setResizable(false); // Prevent resizing the frame
+
+        // Add components to the payment frame
+        JLabel totalAmountLabel = new JLabel("Total Amount: ");
+        JTextField totalAmountField = new JTextField(20);
+        totalAmountField.setText(String.format("%.2f", totalAmount));
+        totalAmountField.setEditable(false);
+
+        JLabel amountPaidLabel = new JLabel("Amount Paid: ");
+        JTextField amountPaidField = new JTextField(20);
+
+        JButton payButton = new JButton("Generate Invoice");
+        JButton cancelOrder = new JButton("Cancel Order");
+        payButton.addActionListener(event -> onPayClicked(totalAmount, amountPaidField.getText(), paymentFrame));
+        cancelOrder.addActionListener(event -> cancellationOrder(paymentFrame));
+
+        paymentFrame.add(totalAmountLabel);
+        paymentFrame.add(totalAmountField);
+        paymentFrame.add(amountPaidLabel);
+        paymentFrame.add(amountPaidField);
+        paymentFrame.add(payButton);
+        paymentFrame.add(cancelOrder);
+
+        // Center the payment frame on the screen
+        paymentFrame.setLocationRelativeTo(null);
+        paymentFrame.setVisible(true);
+    }
+
+    private void cancellationOrder(JFrame paymentFrame) {
+        int confirm = JOptionPane.showConfirmDialog(
+                paymentFrame,
+                "Are you sure you want to cancel this order?",
+                "Cancel Order",
+                JOptionPane.YES_NO_OPTION
+        );
+
+        if (confirm == JOptionPane.YES_OPTION) {
+            try {
+//                orderDao.deleteOrder(orderId);
+                JOptionPane.showMessageDialog(paymentFrame, "Order cancelled successfully.", "Order Cancelled", JOptionPane.INFORMATION_MESSAGE);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+                JOptionPane.showMessageDialog(paymentFrame, "Failed to cancel order", "Error", JOptionPane.ERROR_MESSAGE);
+            } finally {
+                paymentFrame.dispose();
+            }
+        }
+    }
+
+    private double calculateTotalAmount() {
+        double total = 0.0;
+        try {
+             total = cartController.getCartTotal(userId);
+//            // Now you can use this total to display in the UI
+//            JOptionPane.showMessageDialog(this, "Total Cart Amount: " + total, "Cart Total", JOptionPane.INFORMATION_MESSAGE);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Error calculating cart total", "Error", JOptionPane.ERROR_MESSAGE);
+        }
+        return total;
+    }
+
+    private void onPayClicked(double totalAmount, String amountPaidStr, JFrame paymentFrame) {
+        try {
+            double amountPaid = Double.parseDouble(amountPaidStr);
+            if (amountPaid >= totalAmount) {
+                JOptionPane.showMessageDialog(paymentFrame, "Payment successful! Change: " + (amountPaid - totalAmount), "Payment", JOptionPane.INFORMATION_MESSAGE);
+                paymentFrame.dispose(); // Close the payment frame
+            } else {
+                JOptionPane.showMessageDialog(paymentFrame, "Insufficient amount paid.", "Payment Error", JOptionPane.ERROR_MESSAGE);
+            }
+        } catch (NumberFormatException ex) {
+            JOptionPane.showMessageDialog(paymentFrame, "Invalid amount entered.", "Payment Error", JOptionPane.ERROR_MESSAGE);
+        }
     }
 }
