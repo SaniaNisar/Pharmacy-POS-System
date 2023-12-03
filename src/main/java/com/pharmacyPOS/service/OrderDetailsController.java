81b8e2cdecd22a0dffd072af92aa6e38771ca532
Ayesha <ayeshaharoon394@gmail.com>
2023-12-03 07:02:49 +0500

    Commit by Ayesha - OrderDetailController OrderDetailService

--- src/main/java/com/pharmacyPOS/service/OrderDetailsController.java
+++ src/main/java/com/pharmacyPOS/service/OrderDetailsController.java
@@ -1,4 +1,28 @@
-package com.pharmacyPOS.service;
+package com.pharmacyPOS.controllers;
 
-public class OrderDetailsController {
+import com.pharmacyPOS.data.entities.OrderDetail;
+import com.pharmacyPOS.services.OrderDetailService;
+
+import java.util.List;
+
+public class OrderDetailController {
+
+    private final OrderDetailService orderDetailService;
+
+    public OrderDetailController(OrderDetailService orderDetailService) {
+        this.orderDetailService = orderDetailService;
+    }
+
+    // Additional methods can be added based on business logic or requirements
+
+    public void updateOrderDetail(OrderDetail orderDetail) {
+        orderDetailService.updateOrderDetail(orderDetail);
+        // Add any additional logic or response handling if needed
+    }
+
+    public void deleteOrderDetail(int orderDetailId) {
+        orderDetailService.deleteOrderDetail(orderDetailId);
+        // Add any additional logic or response handling if needed
+    }
+
 }
