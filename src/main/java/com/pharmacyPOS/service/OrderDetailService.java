6c7857a821d5d46e77b80de6ac3ad35594b66513
Ayesha <ayeshaharoon394@gmail.com>
2023-12-03 06:59:17 +0500

    Commit by Ayesha - OrderDetailService

--- src/main/java/com/pharmacyPOS/service/OrderDetailService.java
+++ src/main/java/com/pharmacyPOS/service/OrderDetailService.java
@@ -1,5 +1,30 @@
-package com.pharmacyPOS.service;
+package com.pharmacyPOS.services;
+
+import com.pharmacyPOS.data.dao.OrderDetailDao;
+import com.pharmacyPOS.data.entities.OrderDetail;
+
+import java.util.List;
 
 public class OrderDetailService {
 
+    private final OrderDetailDao orderDetailDao;
+
+    public OrderDetailService(OrderDetailDao orderDetailDao) {
+        this.orderDetailDao = orderDetailDao;
+    }
+
+    // Additional methods can be added based on business logic or requirements
+
+    public void updateOrderDetail(OrderDetail orderDetail) {
+        orderDetailDao.updateOrderDetail(orderDetail);
+    }
+
+    public void deleteOrderDetail(int orderDetailId) {
+        orderDetailDao.deleteOrderDetail(orderDetailId);
+    }
+
+    public List<OrderDetail> getAllOrderDetails() {
+        // You can add additional logic or validation if needed
+        return orderDetailDao.getAllOrderDetails();
+    }
 }
