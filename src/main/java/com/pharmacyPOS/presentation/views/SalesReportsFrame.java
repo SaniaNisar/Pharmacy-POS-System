4306f793afc5d22b83b60fba60133834a4df1bbe
Ayesha <ayeshaharoon394@gmail.com>
2023-12-04 00:07:51 +0500

    Commit by Ayesha Sales Reports Charts being created

--- src/main/java/com/pharmacyPOS/presentation/views/SalesReportsFrame.java
+++ src/main/java/com/pharmacyPOS/presentation/views/SalesReportsFrame.java
@@ -8,6 +8,12 @@ import net.sourceforge.jdatepicker.impl.JDatePanelImpl;
 import net.sourceforge.jdatepicker.impl.JDatePickerImpl;
 import net.sourceforge.jdatepicker.impl.UtilDateModel;
 
+import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
+import org.jfree.chart.JFreeChart;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
+
 import javax.swing.*;
 import java.awt.*;
 import java.sql.Date;
@@ -24,12 +30,15 @@ public class SalesReportsFrame extends JFrame {
     private JDatePickerImpl datePickerWeekly;
     private JComboBox<Integer> yearComboBox;
     private JComboBox<String> monthComboBox;
+    private JPanel chartPanelDaily;
+    private JPanel chartPanelWeekly;
+    private JPanel chartPanelMonthly;
 
     public SalesReportsFrame(SalesDao salesDao) {
         this.salesReportGeneration = new SalesReportGeneration(salesDao);
 
         setTitle("Sales Report Generator");
-        setSize(600, 400);
+        setSize(600, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setBackground(new Color(0, 0, 128));
 
@@ -61,10 +70,17 @@ public class SalesReportsFrame extends JFrame {
         topPanel.add(button);
 
         panel.add(topPanel, BorderLayout.NORTH);
+
+        chartPanelDaily = new JPanel(new BorderLayout());
         reportAreaDaily = new JTextArea();
         reportAreaDaily.setEditable(false);
-        panel.add(new JScrollPane(reportAreaDaily), BorderLayout.CENTER);
 
+        // Combine the report area and chart in one panel
+        JPanel combinedPanel = new JPanel(new BorderLayout());
+        combinedPanel.add(new JScrollPane(reportAreaDaily), BorderLayout.CENTER);
+        combinedPanel.add(chartPanelDaily, BorderLayout.SOUTH); // Chart will be added here
+
+        panel.add(combinedPanel, BorderLayout.CENTER);
         return panel;
     }
 
@@ -83,10 +99,17 @@ public class SalesReportsFrame extends JFrame {
         topPanel.add(button);
 
         panel.add(topPanel, BorderLayout.NORTH);
+
+        chartPanelWeekly = new JPanel(new BorderLayout());
         reportAreaWeekly = new JTextArea();
         reportAreaWeekly.setEditable(false);
-        panel.add(new JScrollPane(reportAreaWeekly), BorderLayout.CENTER);
 
+        // Combine the report area and chart in one panel
+        JPanel combinedPanel = new JPanel(new BorderLayout());
+        combinedPanel.add(new JScrollPane(reportAreaWeekly), BorderLayout.CENTER);
+        combinedPanel.add(chartPanelWeekly, BorderLayout.SOUTH); // Chart will be added here
+
+        panel.add(combinedPanel, BorderLayout.CENTER);
         return panel;
     }
 
@@ -99,7 +122,7 @@ public class SalesReportsFrame extends JFrame {
         }
 
         JLabel labelMonth = new JLabel("Select Month:");
-        monthComboBox = new JComboBox<>(new String[] {
+        monthComboBox = new JComboBox<>(new String[]{
                 "January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"
         });
 
@@ -114,10 +137,17 @@ public class SalesReportsFrame extends JFrame {
         topPanel.add(button);
 
         panel.add(topPanel, BorderLayout.NORTH);
+
+        chartPanelMonthly = new JPanel(new BorderLayout());
         reportAreaMonthly = new JTextArea();
         reportAreaMonthly.setEditable(false);
-        panel.add(new JScrollPane(reportAreaMonthly), BorderLayout.CENTER);
 
+        // Combine the report area and chart in one panel
+        JPanel combinedPanel = new JPanel(new BorderLayout());
+        combinedPanel.add(new JScrollPane(reportAreaMonthly), BorderLayout.CENTER);
+        combinedPanel.add(chartPanelMonthly, BorderLayout.SOUTH); // Chart will be added here
+
+        panel.add(combinedPanel, BorderLayout.CENTER);
         return panel;
     }
 
@@ -130,7 +160,7 @@ public class SalesReportsFrame extends JFrame {
 
         Date sqlDate = new Date(selectedDate.getTime());
         List<SalesReport> reports = salesReportGeneration.getDailySalesReport(sqlDate);
-        displayReports(reports, reportAreaDaily);
+        displayReports(reports, reportAreaDaily, chartPanelDaily);
     }
 
     private void generateWeeklyReport() {
@@ -142,29 +172,56 @@ public class SalesReportsFrame extends JFrame {
 
         Date sqlDate = new Date(selectedDate.getTime());
         List<SalesReport> reports = salesReportGeneration.getWeeklySalesReport(sqlDate);
-        displayReports(reports, reportAreaWeekly);
+        displayReports(reports, reportAreaWeekly, chartPanelWeekly);
     }
 
     private void generateMonthlyReport() {
         int year = (int) yearComboBox.getSelectedItem();
         int month = monthComboBox.getSelectedIndex() + 1;
         List<SalesReport> reports = salesReportGeneration.getMonthlySalesReport(year, month);
-        displayReports(reports, reportAreaMonthly);
+        displayReports(reports, reportAreaMonthly, chartPanelMonthly);
     }
 
-    private void displayReports(List<SalesReport> reports, JTextArea reportArea) {
+    private void displayReports(List<SalesReport> reports, JTextArea reportArea, JPanel chartPanel) {
+        // Update text area
         StringBuilder sb = new StringBuilder();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
+        // Dataset for the chart
+        DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
         for (SalesReport report : reports) {
+            // Update text information
             sb.append("Date: ").append(dateFormat.format(report.getSaleDate())).append("\n");
             sb.append("Product ID: ").append(report.getProductId()).append("\n");
             sb.append("Product Name: ").append(report.getProductName()).append("\n");
             sb.append("Total Quantity Sold: ").append(report.getTotalQuantitySold()).append("\n");
             sb.append("Total Sales Amount: $").append(String.format("%.2f", report.getTotalSalesAmount())).append("\n\n");
+
+            // Add data to the dataset for chart
+            dataset.addValue(report.getTotalSalesAmount(), "Sales", report.getProductName());
         }
 
         reportArea.setText(sb.toString());
+
+        // Create chart based on the dataset
+        JFreeChart barChart = ChartFactory.createBarChart(
+                "Sales Chart",
+                "Product",
+                "Sales Amount",
+                dataset,
+                PlotOrientation.VERTICAL,
+                true, true, false);
+
+        // Create a ChartPanel for the chart
+        ChartPanel newChartPanel = new ChartPanel(barChart);
+        newChartPanel.setPreferredSize(new Dimension(600, 300)); // Set preferred size for the chart panel
+
+        // Clear the previous chart and add the new one
+        chartPanel.removeAll();
+        chartPanel.add(newChartPanel);
+        chartPanel.revalidate();
+        chartPanel.repaint();
     }
 
     public static void main(String[] args) {
